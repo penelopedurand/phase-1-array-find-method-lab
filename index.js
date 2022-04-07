ac38a972ba534b215579bb18ecd2c92b1f70f1d2
Penelope Durand <penelope.durand@mail.goucher.edu>
2022-04-07 11:28:47 -0400

    commit tree 7c89ff08ea9dd254f4a4d1b5a7f46a51e1f34d50

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // code your solution here
+superbowlWin = (records) => {
+    const result = records.find(record => record.result === "W")
+        if (result) {
+            return result.year
+        }
+}
